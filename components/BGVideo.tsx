707f2a75bf1ab49f4091ab79b3affa389245bc1c
JusceCabrera <jusce240@gmail.com>
2024-11-25 21:27:10 -0500

    fix text and video

--- components/BGVideo.tsx
+++ components/BGVideo.tsx
@@ -2,7 +2,7 @@
 const BackgroundVideo = () => {
   return (
     <video
-        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10 overflow-hidden"
         autoPlay
         loop
         muted
