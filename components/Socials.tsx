0f871bc22d9ffca5af61020d0c872b4732785624
JusceCabrera <jusce240@gmail.com>
2024-11-30 18:24:40 -0500

    remove logo

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -6,7 +6,7 @@ import Hero from "./Hero"
 const Socials = () => {
   return (
     <div className="font-jetbrains text-white flex flex-row gap-14 w-screen justify-center pt-10">
-      <Hero />
+      {/* <Hero /> */}
       <a href="https://www.instagram.com/eros.fitx" target="blank" className="flex justify-center items-center">
         {/* <Image src={igLogo} alt="iglogo" className="w-10 h-10" /> */}
         CONTACT
