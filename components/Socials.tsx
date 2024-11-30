bdae8459503a69e7b5242be6dea83b68d799e41e
JusceCabrera <jusce240@gmail.com>
2024-11-30 15:18:16 -0500

    change socials page

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,26 +1,20 @@
-import Image from "next/image"
-import igLogo from '../app/assets/igLogo.svg'
-import tiktokLogo from '../app/assets/tiktokLogo.svg'
+// import Image from "next/image"
+// import igLogo from '../app/assets/igLogo.svg'
+// import tiktokLogo from '../app/assets/tiktokLogo.svg'
 import Hero from "./Hero"
 
 const Socials = () => {
   return (
-    <div className="flex flex-row gap-14 w-screen justify-center pt-10">
+    <div className="font-jetbrains text-white flex flex-row gap-14 w-screen justify-center pt-10">
       <Hero />
-      <a href="https://www.instagram.com/eros.fitx" target="blank">
-        <Image 
-          src={igLogo} 
-          alt="iglogo" 
-          className="w-10 h-10"
-        />
+      <a href="https://www.instagram.com/eros.fitx" target="blank" className="flex justify-center items-center">
+        {/* <Image src={igLogo} alt="iglogo" className="w-10 h-10" /> */}
+        CONTACT
       </a>  
        
-      <a href="https://www.instagram.com/eros.fitx" target="blank">
-        <Image 
-          src={tiktokLogo} 
-          alt="tiktoklogo" 
-          className="w-10 h-10"
-        />
+      <a href="https://www.youtube.com/watch?v=oD5f55ohsc4" target="blank" className="flex justify-center items-center">
+        {/* <Image src={tiktokLogo} alt="tiktoklogo" className="w-10 h-10"/> */}
+        WATCH VIDEO
       </a>  
     </div>
   )
